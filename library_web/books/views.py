327216efb4d3b543eb5a3d818f6e8043301666ad
dailycoding82 <147251131+dailycoding82@users.noreply.github.com>
2025-01-08 11:05:15 +0800

    Delete library_web directory

--- library_web/books/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.views.generic import ListView
-from .models import Book
-
-
-class BookListView(ListView):
-  model = Book
-  templte_name = "book_list.html"
